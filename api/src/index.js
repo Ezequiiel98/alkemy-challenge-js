814867f3930189847f55ff6c33f7a7e73552525a
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-01-12 19:57:10 -0300

    agrego configuraciones basicas de la api y de la base de datos

---
+++ api/src/index.js
@@ -0,0 +1,27 @@
+const cors = require('cors');
+const express = require('express');
+
+const database = require('./config/database');
+
+const PORT_APP = process.env.PORT || 3001;
+const app = express();
+
+if (process.env.NODE_ENV === 'dev') {
+  const dotenv = require('dotenv');
+  const morgan = require('morgan');
+
+  dotenv.config();
+  app.use(morgan('dev'));
+}
+
+// db connection
+database.sync()
+  .then(() => console.log('database is working'))
+  .catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT_APP, () => {
+  console.log(`Server on port ${PORT_APP}`);
+});

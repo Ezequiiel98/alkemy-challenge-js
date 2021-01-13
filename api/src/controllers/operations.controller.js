efa99acf13e300ce239fe974e13b48f7797ef869
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-01-13 05:10:48 -0300

    agrego controllers para editar y borrar operacion

--- api/src/controllers/operations.controller.js
+++ api/src/controllers/operations.controller.js
@@ -1,9 +1,12 @@
 const Operations = require('../models/Operations.model');
+const User = require('../models/Users.model');
 
 exports.createOperation = async (req, res) => {
   const {
     amount, date, description, type,
   } = req.body;
+
+  // const user = await User.findOne({ where: { id: userId } });
 
   await Operations.create({
     amount, date, description, type,
@@ -26,5 +29,31 @@ exports.getOperation = async (req, res) => {
   return res.status(200).json(operation);
 };
 
-exports.updateOperation = async (req, res) => res.send('put');
-exports.deleteOperation = async (req, res) => res.send('delete');
+exports.updateOperation = async (req, res) => {
+  const { id } = req.params;
+  const {
+    amount, date, description, type,
+  } = req.body;
+
+  const newData = {
+    amount, date, description, type,
+  };
+
+  const operationExist = await Operations.findOne({ where: { id } });
+
+  if (!operationExist) {
+    return res.status(404).json({ message: 'Operation not found' });
+  }
+
+  await Operations.update(newData, { where: { id } });
+
+  return res.status(200).json({ message: 'Operation updated successfully' });
+};
+
+exports.deleteOperation = async (req, res) => {
+  const { id } = req.params;
+
+  await Operations.destroy({ where: { id } });
+
+  res.status(200).send('Operation deleted successfully');
+};

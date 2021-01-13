a496e882c5447c5f493f9d2d44e360f8eb4a6b69
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-01-13 01:34:54 -0300

    agrego el controlador para logout

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -37,3 +37,15 @@ exports.login = async (req, res) => {
 
   return res.status(200).json({ username: user.username, email, token });
 };
+
+exports.logout = async (req, res) => {
+  const { token } = req.params;
+
+  if (!token) {
+    return res.status(400).json({ message: 'Token is required' });
+  }
+
+  await ValidTokens.destroy({ where: { token } });
+
+  return res.status(200).json({ message: 'logout' });
+};
--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 
 const { controllerWithTryCatch } = require('../helpers');
-const { signUp, login } = require('../controllers/auth.controller');
+const { signUp, login, logout } = require('../controllers/auth.controller');
 
 const router = Router();
 
@@ -9,6 +9,6 @@ router.post('/signup', controllerWithTryCatch(signUp));
 
 router.post('/login', controllerWithTryCatch(login));
 
-router.delete('/logout', (req, res) => res.send('logout'));
+router.delete('/logout/:token', controllerWithTryCatch(logout));
 
 module.exports = router;

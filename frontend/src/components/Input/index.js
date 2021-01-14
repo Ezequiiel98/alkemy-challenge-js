1efffd97947b9c89d0a44a86600c0ff2ed213b49
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-01-14 03:59:36 -0300

    agrego componente Input y empiezo cona hacer el form para el login

---
+++ frontend/src/components/Input/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import styles from './index.module.scss';
+
+function Input({
+  label, id, type, name, error, onClick, onChange, value, required,
+}) {
+  return (
+    <>
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+      <div className={styles.containerInput}>
+        <input
+          className={styles.input}
+          type={type}
+          id={id}
+          onClick={onClick}
+          onChange={onChange}
+          value={value}
+          required={required}
+          name={name}
+        />
+        { error && (
+          <div className={styles.containerErrors}>
+            <span className={styles.error}>
+              {' '}
+              { error }
+              {' '}
+            </span>
+          </div>
+        ) }
+      </div>
+    </>
+  );
+}
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  error: PropTypes.string,
+  onClick: PropTypes.func,
+  onChange: PropTypes.func,
+  value: PropTypes.string,
+  required: PropTypes.bool,
+};
+
+Input.defaultProps = {
+  type: 'text',
+  error: null,
+  onClick: null,
+  onChange: null,
+  value: '',
+  required: false,
+};
+
+export default Input;
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import Input from '../../components/Input';
+import bgLogin from '../../assets/img/login-bg.jpg';
+
+import styles from './index.module.scss';
+
 function Login() {
   return (
-    <h1>login</h1>
-  ); 
+    <div className={styles.containerLogin}>
+      <div className={styles.containerBg}>
+        <img src={bgLogin} alt="" className={styles.bgLogin} />
+      </div>
+      <div className={styles.containerForm}>
+        <form className={styles.form} action="">
+          <Input
+            type="email"
+            label="email"
+            id="email"
+            name="email"
+            error="Pass incorrect"
+          />
+
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;

383e5f6291e83e990def911a326098a3c30cf8fa
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-01-15 02:23:38 -0300

    agrego validacion de token en el home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
-
+import React, { useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { validateTokenService } from '../../services/auth.service';
+import { AuthContext } from '../../context/AuthContext';
 import Menu from '../../components/Menu';
 
 import styles from './index.module.scss';
 
-function Home() {
+function Home(props) {
+  const [dataAuth, setDataAuth] = useContext(AuthContext);
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const { data: { username, email } } = await validateTokenService(dataAuth.token);
+        setDataAuth({ ...dataAuth, username, email });
+      } catch {
+        localStorage.removeItem('token');
+        setDataAuth({ });
+        props.history.push('/login');
+      }
+    };
+    validateToken();
+  });
+
   return (
     <div className={styles.containerHome}>
       <Menu />
     </div>
   );
 }
+
+Home.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Home;
--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -5,3 +5,5 @@ export const loginService = (data) => api.post('/auth/login', data);
 export const signUpService = (data) => api.post('/auth/signup', data);
 
 export const logoutService = (token) => api.delete(`/auth/logout/${token}`);
+
+export const validateTokenService = (token) => api.get(`/auth/validate-token/${token}`);

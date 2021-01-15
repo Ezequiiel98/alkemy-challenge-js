713c0e705813f20c8af8000f983195dc824abd6b
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-01-15 02:02:43 -0300

    agrego funcion para saber el total del gasto entradas y lo que queda

--- api/src/controllers/operations.controller.js
+++ api/src/controllers/operations.controller.js
@@ -22,6 +22,10 @@ exports.getOperations = async (req, res) => {
   const {
     limit, indexPage, offset, order,
   } = pagination({ perPage, last, page });
+  const sumAmountOperations = {
+    spend: 0,
+    entry: 0,
+  };
 
   const operations = await Operations.findAll({
     attributes: { exclude: ['userId'] },
@@ -31,73 +35,16 @@ exports.getOperations = async (req, res) => {
     order,
   });
 
-  res.status(200).json({ page: indexPage, per_page: limit, operations });
-};
+  const allOperations = await Operations.findAll({ where: { userId } });
 
-// by type
-exports.getOperationsByType = async (req, res, next, type) => {
-  const { per_page: perPage, page, last } = req.query;
-  const { userId } = req.body;
-  const {
-    limit, indexPage, offset, order,
-  } = pagination({ perPage, last, page });
-
-  const operations = await Operations.findAll({
-    attributes: { exclude: ['userId'] },
-    where: { userId, type },
-    limit,
-    offset,
-    order,
+  allOperations.forEach((operation) => {
+    sumAmountOperations[operation.type] += parseInt(operation.amount, 10);
   });
 
-  res.status(200).json({ page: indexPage, per_page: limit, operations });
-};
-
-// get operation by id
-exports.getOperation = async (req, res) => {
-  const { id } = req.params;
-  const { userId } = req.body;
-
-  const operation = await Operations.findOne({
-    where: { id, userId },
-    attributes: { exclude: ['userId'] },
+  res.status(200).json({
+    money: { ...sumAmountOperations, rest: sumAmountOperations.entry - sumAmountOperations.spend },
+    page: indexPage,
+    per_page: limit,
+    operations,
   });
-
-  if (!operation) {
-    return res.status(404).json({ message: 'Operation not found' });
-  }
-
-  return res.status(200).json(operation);
-};
-
-// update
-exports.updateOperation = async (req, res) => {
-  const { id } = req.params;
-  const {
-    amount, date, description, userId,
-  } = req.body;
-
-  const newData = {
-    amount, date, description,
-  };
-
-  const operationExist = await Operations.findOne({ where: { id, userId } });
-
-  if (!operationExist) {
-    return res.status(404).json({ message: 'Operation not found' });
-  }
-
-  await Operations.update(newData, { where: { id } });
-
-  return res.status(200).json({ message: 'Operation updated successfully' });
-};
-
-// delete operation
-exports.deleteOperation = async (req, res) => {
-  const { id } = req.params;
-  const { userId } = req.body;
-
-  await Operations.destroy({ where: { id, userId } });
-
-  res.status(200).send({ message: 'Operation deleted successfully' });
 };
